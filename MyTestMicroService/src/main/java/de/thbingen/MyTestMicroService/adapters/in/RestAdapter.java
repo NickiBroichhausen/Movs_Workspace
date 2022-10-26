ff0886b0dc3473d8265bfb896fa1cbe6049b0ecc
Provashionell <68772143+NickiBroichhausen@users.noreply.github.com>
2022-10-26 17:47:20 +0200

    MyTestMicroServices: autowire corrected

--- MyTestMicroService/src/main/java/de/thbingen/MyTestMicroService/adapters/in/RestAdapter.java
+++ MyTestMicroService/src/main/java/de/thbingen/MyTestMicroService/adapters/in/RestAdapter.java
@@ -1,14 +1,15 @@
 package de.thbingen.MyTestMicroService.adapters.in;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.thbingen.MyTestMicroService.ports.in.HttpmsPort;
-import de.thbingen.MyTestMicroService.service.Service;
 
 @RestController
 public class RestAdapter{
-	private HttpmsPort in = new Service();
+	@Autowired
+	private HttpmsPort in;
 	
 	@GetMapping("/hallo")
 	public void safeData(String name) {
--- MyTestMicroService/src/main/java/de/thbingen/MyTestMicroService/service/Service.java
+++ MyTestMicroService/src/main/java/de/thbingen/MyTestMicroService/service/Service.java
@@ -1,13 +1,17 @@
 package de.thbingen.MyTestMicroService.service;
 
-import de.thbingen.MyTestMicroService.adapters.out.PostgresStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import de.thbingen.MyTestMicroService.ports.in.HttpmsPort;
 import de.thbingen.MyTestMicroService.ports.out.MessageRepositoryPort;
 
 @org.springframework.stereotype.Service
 public class Service implements HttpmsPort{
+	@Autowired
+	private MessageRepositoryPort out;
+	
     public void safeData(String name) {
-    	MessageRepositoryPort out = new PostgresStorage();
+
     	out.storeMessage(name);
     }
 }
