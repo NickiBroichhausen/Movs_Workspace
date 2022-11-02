faa84b09857e37b595d5968c857a288d133537ab
Nicki Leon Broichhausen <68772143+NickiBroichhausen@users.noreply.github.com>
2022-11-02 20:09:38 +0100

    RestUebung: get,post,delete  connected to csv

--- RestUebung/src/main/java/de/thBingen/U5/RestUebung/adapters/in/RestController.java
+++ RestUebung/src/main/java/de/thBingen/U5/RestUebung/adapters/in/RestController.java
@@ -4,6 +4,7 @@ import de.thBingen.U5.RestUebung.ports.in.RestPort;
 import de.thBingen.U5.RestUebung.ports.in.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @org.springframework.web.bind.annotation.RestController
@@ -21,13 +22,16 @@ public class RestController {
     }
 
     @PostMapping(value="/users", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addUser(@RequestBody User user) {
-       restPort.postUser(user);
+    public ResponseEntity addUser(@RequestBody User user) {
+       boolean success = restPort.postUser(user);
+        return success==true?ResponseEntity.ok(user.getId()):ResponseEntity.badRequest().build();
     }
+    //id's might be used multiple times
 
     @DeleteMapping(value = "/users/{id}")
-    public void deleteUser(@PathVariable int id){
-        restPort.deleteUser(id);
+    public ResponseEntity deleteUser(@PathVariable int id){
+        boolean success = restPort.deleteUser(id);
+        return success==true?ResponseEntity.ok(id):ResponseEntity.badRequest().build();
     }
 
 
--- RestUebung/src/main/java/de/thBingen/U5/RestUebung/adapters/out/CsvStorage.java
+++ RestUebung/src/main/java/de/thBingen/U5/RestUebung/adapters/out/CsvStorage.java
@@ -14,16 +14,15 @@ public class CsvStorage implements CsvPort {
     public boolean addUser(User user) {
         System.out.println(user);
         try {
-//            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-//            in.readLine();
             FileWriter fw = new FileWriter(file, true);
             BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(user.toString());
+            bw.write(user.getId() + "," + user.getName() + "," + user.getSurname());
             bw.newLine();
             bw.close();
         }
         catch(Exception e){
             e.printStackTrace();
+            return false;
         }
         return true;
     }
@@ -31,12 +30,55 @@ public class CsvStorage implements CsvPort {
     @Override
     public User getUser(int id) {
         System.out.println("getting: " + id);
-        return null;
+        User user = null;
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+            String next;
+            while((next = in.readLine()) != null){
+                if(next.startsWith(id+",")){
+                    String[] data= next.split(",");
+                    user = new User(id, data[1], data[2]);
+                }
+            }
+            in.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(user);
+        return user;
     }
 
     @Override
     public boolean deleteUser(int id) {
         System.out.println("deleting: " + id);
-        return true;
+        boolean successful = false;
+
+        try {
+            File target_file = new File(this.file);
+            File tmp_file = new File("temp.csv");
+
+            BufferedReader reader = new BufferedReader(new FileReader(target_file));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tmp_file));
+
+            String currentLine;
+
+            while((currentLine = reader.readLine()) != null) {
+                if(currentLine.startsWith(id+","))
+                    continue;
+                writer.write(currentLine + System.getProperty("line.separator"));
+            }
+            writer.close();
+            reader.close();
+            System.out.println("heir");
+            boolean test = target_file.delete();
+            System.out.println(test);
+            successful = tmp_file.renameTo(target_file);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return successful;
     }
 }
